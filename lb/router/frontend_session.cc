e6e99071785aaa3c09a27a189d1dfafc05bb90a4
Conzxy <1967933844@qq.com>
2022-09-09 00:21:29 +0800

    Send normal node

--- lb/router/frontend_session.cc
+++ lb/router/frontend_session.cc
@@ -19,17 +19,21 @@ FrontendSession::FrontendSession(LoadBalancer *lb, TcpConnectionPtr const &conn)
     auto *backend = GetPointer(lb_->pt_backends_[index]);
     
     BackendSession *current_session = nullptr;
-    {
+    { 
     MutexGuard g(backend->lock);
-
-    if (backend->current == backend->backends.size()) backend->current = 0;
-    current_session = GetPointer(backend->backends[backend->current]);
-    auto &header_map = request.headers;
-    auto iter = header_map.find("Host");
-    if (iter != header_map.end())
-      iter->second = lbconfig().backend_addrs[backend->current].ToIpPort();
-    backend->current++;
+    
+    for (;;) {
+      if (backend->current == backend->backends.size()) backend->current = 0;
+      current_session = GetPointer(backend->backends[backend->current]);
+      auto &header_map = request.headers;
+      auto iter = header_map.find("Host");
+      if (iter != header_map.end())
+        iter->second = lbconfig().backend_addrs[backend->current++].ToIpPort();
+      if (current_session->Send(_conn, codec_, request)) {
+        LOG_INFO << "Select the server index: " << backend->current-1;
+        break;
+      }
+    } // lock
     }
-    current_session->Send(_conn, codec_, request);
   });
 }
