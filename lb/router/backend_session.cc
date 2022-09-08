7997df93c9262344fdf08f9a87b3c34a0b1dded6
Conzxy <1967933844@qq.com>
2022-09-08 22:32:57 +0800

    backend session(forward response)

--- lb/router/backend_session.cc
+++ lb/router/backend_session.cc
@@ -1,11 +1,23 @@
 #include "backend_session.h"
 
-#include "lb/http/http_request_codec.h"
+#include "kanon/log/logger.h"
 #include "lb/http/http_request.h"
+#include "lb/http/http_request_codec.h"
+
+#undef PARSER_DEBUG
+#define PARSER_DEBUG 1
+
+#if PARSER_DEBUG
+#include <iostream>
+#endif
 
 using namespace lb;
 using namespace kanon;
 using namespace http;
+
+#define LOG_SEND_DIRECTION(frontend, backend)                                  \
+  LOG_INFO << "Send" << frontend->GetPeerAddr().ToIpPort() << "->"             \
+           << backend->GetPeerAddr().ToIpPort();
 
 BackendSession::BackendSession(EventLoop *loop, InetAddr const &backend_addr,
                                std::string const &name)
@@ -25,25 +37,45 @@ BackendSession::BackendSession(EventLoop *loop, InetAddr const &backend_addr,
 
   backend_->SetMessageCallback(
       [this](TcpConnectionPtr const &conn, Buffer &buffer, TimeStamp) {
-        if (parser_.Parse(buffer) == HttpResponseParser::PARSE_OK) {
-          assert(!frontends_.empty());
-          auto frontend = std::move(frontends_.back());
-          frontends_.pop_back();
+#if PARSER_DEBUG
+#endif
+        for (;;) {
+          switch (parser_.Parse(buffer)) {
+          case HttpResponseParser::PARSE_OK: {
+            LOG_DEBUG << "Parse OK";
+            assert(!frontends_.empty());
+            auto frontend = GetPointer(frontends_.back());
+            if (!parser_.IsChunked()) frontends_.pop_back();
 
-          std::string response = buffer.RetrieveAsString(parser_.index());
-          parser_.ResetIndex();
-          frontend->Send(response);
+            std::string response = buffer.RetrieveAsString(parser_.index());
+            parser_.ResetIndex();
+
+#if PARSER_DEBUG
+            LOG_DEBUG << "Response: ";
+#endif
+            LOG_DEBUG << "Response length = " << response.size();
+
+            LOG_SEND_DIRECTION(conn, frontend);
+            frontend->Send(response);
+          } break;
+
+          case HttpResponseParser::PARSE_ERR: {
+            LOG_ERROR << "Response parse error";
+            return;
+          }
+
+          case HttpResponseParser::PARSE_SHORT: {
+            LOG_DEBUG << "Parse short";
+            return;
+          }
+          }
+          // FIXME
+          // Error handling
         }
-        // FIXME 
-        // Error handling
       });
 
   backend_->EnableRetry();
 }
-
-#define LOG_SEND_DIRECTION(frontend, backend)                                  \
-  LOG_INFO << "Send" << frontend->GetPeerAddr().ToIpPort() << "->"             \
-           << backend->GetPeerAddr().ToIpPort();
 
 void BackendSession::Send(TcpConnectionPtr const &frontend,
                           OutputBuffer &buffer)
@@ -66,8 +98,7 @@ void BackendSession::Send(TcpConnectionPtr const &frontend, Buffer &buffer)
 }
 
 void BackendSession::Send(TcpConnectionPtr const &frontend,
-                          HttpRequestCodec &codec,
-                          HttpRequest &request)
+                          HttpRequestCodec &codec, HttpRequest &request)
 {
   LOG_SEND_DIRECTION(frontend, conn_);
   if (conn_) {
