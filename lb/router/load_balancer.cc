dd640fd4b2819ab8ab5a1a87e78f6454f31135b8
Conzxy <1967933844@qq.com>
2022-09-14 13:42:26 +0800

    Setup frontend and backend

--- lb/router/load_balancer.cc
+++ lb/router/load_balancer.cc
@@ -2,34 +2,59 @@
 
 #include "backend_session.h"
 #include "frontend_session.h"
+#include "lb/router/config.h"
+#include "lb/util/string_util.h"
 
 using namespace kanon;
 using namespace lb;
 
+LoadBalancer::~LoadBalancer() noexcept 
+{
+
+}
+
+// LoadBalancer::PerThreadData::~PerThreadData() noexcept
+// {
+
+// }
+
 LoadBalancer::LoadBalancer(EventLoop *loop, InetAddr const &listen_addr,
-                           std::vector<InetAddr> init_servers)
+                           std::vector<ServerConfig> init_servers)
   : server_(loop, listen_addr, "LoadBalancer")
   , backends_(std::move(init_servers))
 {
-  LOG_INFO << "Init backends: ";
-  for (auto &backend : backends_) {
-    LOG_INFO << backend.ToIpPort();
-  }
+  // pt_backends_.reserve(1);
 
-  server_.SetThreadInitCallback([this](EventLoop *io_loop) {
-    PerThreadData *data = new PerThreadData;
-    auto &index = tl_index_.value();
-    index = pt_backends_.size();
-    for (size_t i = 0; i < backends_.size(); ++i) {
-      data->backends.emplace_back(new BackendSession(
-          io_loop, backends_[i],
-          "BackendSession #" + std::to_string(index) +
-              "-" + std::to_string(i)));
-      LOG_INFO << "backend[" << i << "] start connect";
-      data->backends.back()->Connect();
+  // server_.SetThreadInitCallback([this](EventLoop *io_loop) {
+  //   PerThreadData *data = new PerThreadData;
+  //   auto &index = tl_index_.value();
+  //   index = pt_backends_.size();
+  //   pt_backends_.emplace_back(data);
+  //   for (size_t i = 0; i < backends_.size(); ++i) {
+  //     data->backends.emplace_back(new BackendSession(
+  //         io_loop, backends_[i].addr,
+  //         util::StrCat("BackendSession #", std::to_string(index), 
+  //             "-", std::to_string(i)), i, *pt_backends_[index]));
+  //     LOG_INFO << "backend[" << i << "] start connect";
+  //     data->backends.back()->Connect();
+  //   }
+  // });
+  
+  // Init balancing algorithm configuration
+  switch (lbconfig().bl_algo_type) {
+    case BlAlgoType::ROUND_ROBIN:
+    {
+
+    } break;
+
+    case BlAlgoType::CONSISTENT_HASHING:
+    {
+      for (size_t i = 0; i < backends_.size(); ++i) {
+        auto &config = backends_[i];
+        chash_.AddServer(i, config.name, config.vnode);
+      }
     }
-    pt_backends_.emplace_back(data);
-  });
+  }
 
 #define LOG_ENDPOINT(conn)                                                     \
   LOG_INFO << conn->GetPeerAddr().ToIpPort() << "->"                           \
@@ -38,14 +63,55 @@ LoadBalancer::LoadBalancer(EventLoop *loop, InetAddr const &listen_addr,
   server_.SetConnectionCallback([this](TcpConnectionPtr const &conn) {
     if (conn->IsConnected()) {
       LOG_ENDPOINT(conn) << " UP";
-      conn->SetContext(new FrontendSession(this, conn));
+      int index = 0;
+      switch (lbconfig().bl_algo_type) {
+        case BlAlgoType::ROUND_ROBIN:
+        {
+          if (current_ == backends_.size())
+              current_ = 0;
+          index = current_;
+          ++current_;
+        } break;
+        case BlAlgoType::CONSISTENT_HASHING:
+        {
+          index = chash_.QueryServer(conn->GetPeerAddr().ToIpPort());
+        }
+      }
+      // LOG_DEBUG << "tl_index = " << tl_index_.value();
+      // conn->SetContext(new FrontendSession(this, conn, *pt_backends_[tl_index_.value()]));
+      auto backend_name = util::StrCat(conn->GetName(), " backend");
+      auto frontend_session = new FrontendSession(conn, index, *this);
+      auto backend_session =  new BackendSession(conn->GetLoop(), backends_[index].addr, backend_name, conn, *frontend_session, index, *this);
+      backend_map_.emplace(backend_name, backend_session);
+      frontend_session->SetBackendSession(backend_session);
+      conn->SetContext(frontend_session);
     } else {
+      LOG_DEBUG << conn->GetName() << " disconnection handler";
       LOG_ENDPOINT(conn) << " DOWN";
-      delete *AnyCast<FrontendSession*>(conn->GetContext());
+      
+      auto frontend_session = *AnyCast<FrontendSession*>(conn->GetContext());
+      auto backend_conn = frontend_session->GetBackendConnection();
+      LOG_DEBUG << "Frontend conn ref cnt = " << conn.use_count();
+      if (backend_conn && backend_conn->IsConnected()) {
+        LOG_TRACE << "FrontendSession is disconnected early";
+        LOG_DEBUG << "Backend conn ref cnt = " << backend_conn.use_count();
+        // backend_session.Disconnect();
+        backend_conn->ShutdownWrite();
+        // conn->GetLoop()->QueueToLoop([backend_conn]() {
+        //   if (backend_conn->IsConnected())
+        //     backend_conn->ForceClose();
+        // });
+      } 
+      delete frontend_session;
+      // else {
+      //   LOG_DEBUG << "Frontend conn ref cnt = " << conn.use_count();
+        // conn->GetLoop()->QueueToLoop([frontend_session, conn]() {
+        //   delete frontend_session;
+        //   conn->SetContext(nullptr);
+        //   LOG_ERROR << "Frontend conn ref cnt = " << conn.use_count();
+        // });
+      // }
     }
   });
-
-  // server_.SetMessageCallback(
-  //     [this](TcpConnectionPtr const &conn, Buffer &buffer, TimeStamp) {
-  //     });
 }
+
--- lb/router/load_balancer.h
+++ lb/router/load_balancer.h
@@ -2,30 +2,39 @@
 #define LB_LOAD_BALANCER_H__
 
 #include "kanon/net/user_server.h"
+#include "backend_session.h"
 #include "kanon/thread/thread_local.h"
 
-#include "backend_session.h"
+#include "lb/algo/consistent_hash.h"
+#include "config.h"
 
 namespace lb {
 
 class BackendSession;
+class FrontendSession;
 
 class LoadBalancer : kanon::noncopyable {
   friend class FrontendSession;
+  friend class BackendSession;
 
-  struct PerThreadData {
-    kanon::MutexLock lock;
-    std::vector<std::unique_ptr<BackendSession>> backends;
-    size_t current = 0;
-  };
+  // struct PerThreadData {
+  //   kanon::MutexLock lock;
+  //   std::vector<std::unique_ptr<BackendSession>> backends;
+  //   size_t current = 0;
+  //   ConsistentHash chash;
 
+  //   ~PerThreadData() noexcept;
+  // };
  public:
+
   LoadBalancer(EventLoop *loop, InetAddr const &listen_addr,
-               std::vector<InetAddr> init_servers);
+               std::vector<ServerConfig> init_servers);
+
+  ~LoadBalancer() noexcept;
 
   void SetLoopNum(uint32_t num)
   {
-    pt_backends_.reserve(num);
+    // pt_backends_.reserve(num);
     server_.SetLoopNum(num);
   }
   
@@ -33,12 +42,22 @@ class LoadBalancer : kanon::noncopyable {
   {
     server_.StartRun();
   }
+  
+  ServerConfig const &GetServerConfig(size_t index) const
+  {
+    return backends_[index];
+  }
 
  private:
   TcpServer server_;
-  std::vector<InetAddr> backends_;
-  std::vector<std::unique_ptr<PerThreadData>> pt_backends_;
-  kanon::ThreadLocal<size_t> tl_index_;
+
+  kanon::MutexLock backend_lock_;
+  std::vector<ServerConfig> backends_;
+  size_t current_ = 0;
+  ConsistentHash chash_;
+  std::unordered_map<std::string, std::unique_ptr<BackendSession>> backend_map_;
+  // std::vector<std::unique_ptr<PerThreadData>> pt_backends_;
+  // kanon::ThreadLocal<size_t> tl_index_;
 };
 } // namespace lb
 
